61ba797fe742764a7164bb97d082113f236ee7e8
Celve <celve03@gmail.com>
2022-11-29 08:39:17 +0800

    fix(script): fix path dependencies in fpga scripts

--- riscv/script/run_test_fpga.sh
+++ riscv/script/run_test_fpga.sh
@@ -2,12 +2,12 @@
 # build testcase
 ./build_test.sh $@
 # copy test input
-if [ -f ./testcase/$@.in ]; then cp ./testcase/$@.in ./test/test.in; fi
+if [ -f ../testcase/fpga/$@.in ]; then cp ../testcase/fpga/$@.in ../testspace/test.in; fi
 # copy test output
-if [ -f ./testcase/$@.ans ]; then cp ./testcase/$@.ans ./test/test.ans; fi
+if [ -f ../testcase/fpga/$@.ans ]; then cp ../testcase/fpga/$@.ans ../testspace/test.ans; fi
 # add your own test script here
 # Example: assuming serial port on /dev/ttyUSB1
-./ctrl/build.sh
-./ctrl/run.sh ./test/test.bin ./test/test.in /dev/ttyUSB1 -I
+../fpga/build.sh
+../fpga/run.sh ../testspace/test.bin ../testspace/test.in /dev/ttyUSB1 -I
 #./ctrl/run.sh ./test/test.bin ./test/test.in /dev/ttyUSB1 -T > ./test/test.out
 #if [ -f ./test/test.ans ]; then diff ./test/test.ans ./test/test.out; fi
