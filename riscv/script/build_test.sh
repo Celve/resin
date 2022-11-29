3cda8ba67c975310e4ed68fb6c12635bb1b4578e
Celve <celve03@gmail.com>
2022-11-29 08:50:22 +0800

    fix(scripts): fix test to testspace

--- riscv/script/build_test.sh
+++ riscv/script/build_test.sh
@@ -3,12 +3,12 @@ set -e
 prefix='/opt/riscv'
 rpath=$prefix/bin/
 # clearing test dir
-rm -rf ../test
-mkdir ../test
+rm -rf ../testspace
+mkdir ../testspace
 # compiling rom
 ${rpath}riscv32-unknown-elf-as -o ../sys/rom.o -march=rv32i ../sys/rom.s
 # compiling testcase
-cp ./testcase/${1%.*}.c ./test/test.c
+cp ../testcase/fpga/${1%.*}.c ../testspace/test.c
 ${rpath}riscv32-unknown-elf-gcc -o ../testspace/test.o -I ../sys -c ../testspace/test.c -O2 -march=rv32i -mabi=ilp32 -Wall
 # linking
 ${rpath}riscv32-unknown-elf-ld -T ../sys/memory.ld ../sys/rom.o ../testspace/test.o -L $prefix/riscv32-unknown-elf/lib/ -L $prefix/lib/gcc/riscv32-unknown-elf/10.1.0/ -lc -lgcc -lm -lnosys -o ../testspace/test.om
